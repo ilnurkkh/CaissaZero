0937697b965502f2f7702461f63893906de32908
ilnurkkh <ilnurkkh@gmail.com>
2026-06-29 22:45:36 +0530

    docs(position): Improved comments

--- include/position.hpp
+++ include/position.hpp
@@ -23,38 +23,25 @@ class Position {
  public:
   Position() = default;
 
-  Bitboard getPieces(Piece piece) const {
-    return pieces[piece];
-  }  // Get bitboard for a specific piece
-  Bitboard getPiecesByColor(Color color) const {
-    return byColor[color];
-  }  // Get bitboard for all pieces of a color
-  Bitboard getPiecesByType(PieceType type) const {
-    return byType[type];
-  }  // Get bitboard for all pieces of a type
-  Color getSideToMove() const { return sideToMove; }  // Get the side to move
-  Square getEnPassantSquare() const {
-    return enPassantSquare;
-  }  // Get the en passant target square
-  CastlingRights getCastlingRights() const {
-    return castlingRights;
-  }  // Get current castling rights
+  Bitboard getPieces(Piece piece) const { return pieces[piece]; }
+  Bitboard getPiecesByColor(Color color) const { return byColor[color]; }
+  Bitboard getPiecesByType(PieceType type) const { return byType[type]; }
+  Color getSideToMove() const { return sideToMove; }
+  Square getEnPassantSquare() const { return enPassantSquare; }
+  CastlingRights getCastlingRights() const { return castlingRights; }
 
   void setPiece(Piece piece, Square sq) {
     Bitboard bb = squareBB(sq);
     pieces[piece] |= bb;
-    byColor[static_cast<Color>(piece / 6)] |= bb;  // Set the bit for this color
-    byType[static_cast<PieceType>(piece % 6)] |=
-        bb;  // Set the bit for this piece type
+    byColor[static_cast<Color>(piece / 6)] |= bb;
+    byType[static_cast<PieceType>(piece % 6)] |= bb;
   }
 
   void removePiece(Piece piece, Square sq) {
     Bitboard bb = squareBB(sq);
     pieces[piece] &= ~bb;
-    byColor[static_cast<Color>(piece / 6)] &=
-        ~bb;  // Clear the bit for this color
-    byType[static_cast<PieceType>(piece % 6)] &=
-        ~bb;  // Clear the bit for this piece type
+    byColor[static_cast<Color>(piece / 6)] &= ~bb;
+    byType[static_cast<PieceType>(piece % 6)] &= ~bb;
   }
 
   Piece getPieceAt(Square sq) const {
@@ -67,6 +54,7 @@ class Position {
     return PIECE_NONE;
   }
 
+  // Set the position from a FEN string
   void setFEN(const std::string &fen) {
     // Clear the current position
     pieces.fill(0);
